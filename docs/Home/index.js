116b235a9d0588be3ef436cf5cde1e5d23c496e9
Alan King <53439159+akingkci@users.noreply.github.com>
2021-03-18 18:05:59 -0400

    Update index.js

--- docs/Home/index.js
+++ docs/Home/index.js
@@ -24,7 +24,34 @@ const Foo = () => (
       `}
     </Code>
   </div>
-  <p>First: <input type='text'>-</p>
+ 
+  
+  render() {
+    return (
+      <form>
+        <label>
+          Is going:
+          <input
+            name="isGoing"
+            type="checkbox"
+            checked={this.state.isGoing}
+            onChange={this.handleInputChange} />
+        </label>
+        <br />
+        <label>
+          Number of guests:
+          <input
+            name="numberOfGuests"
+            type="number"
+            value={this.state.numberOfGuests}
+            onChange={this.handleInputChange} />
+        </label>
+      </form>  
+  
+  
+  
+  
+  
 );
 
 export default Home;
