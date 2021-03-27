f934491fd6ab46cb9c6ab0c259cad1490de5f210
Alan King <53439159+akingkci@users.noreply.github.com>
2021-03-27 13:46:38 -0400

    Update demo page at 1:46p

--- docs/Home/index.js
+++ docs/Home/index.js
@@ -3,7 +3,7 @@ import { Code } from '@deque/cauldron-react';
 
 const Home = () => (
   <div>
-    <h1>Cauldron React: Updated 3/27 01:09p</h1>
+    <h1>Cauldron React: Updated 3/27 01:46p</h1>
     <h2>Installation</h2>
     <h3></h3>
     <Code>
