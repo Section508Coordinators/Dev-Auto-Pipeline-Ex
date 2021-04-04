c3e005f17a693c236caee59a917e891113da0926
Alan King <53439159+akingkci@users.noreply.github.com>
2021-04-04 16:04:10 -0400

    Turned on region for unit testing

--- packages/react/__tests__/axe.js
+++ packages/react/__tests__/axe.js
@@ -89,7 +89,7 @@ const axe = configureAxe({
 		 	'p-as-heading': {enabled: false},
 		 	'page-has-heading-one': {enabled: false},
 		 	'radiogroup': {enabled: false},
-		 	'region': {enabled: false},		 	
+		 	'region': {enabled: true},		 	
 		 	'server-side-image-map': {enabled: false},
 		 	'skip-link': {enabled: false},
 		 	'tabindex': {enabled: false},
