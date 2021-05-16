fbc076b8c5083ecae8161e954bc6e8b38ec640e1
Alan King <53439159+akingkci@users.noreply.github.com>
2021-05-16 15:27:54 -0400

    Updated home index.js home page

--- docs/Home/index.js
+++ docs/Home/index.js
@@ -3,7 +3,7 @@ import { Code } from '@deque/cauldron-react';
 
 const Home = () => (
   <div>
-    <h1>Cauldron React: Demo example site</h1>
+    <h1>Cauldron React: Demo example site: Code change here</h1>
     <h2>Installation</h2>
     <h3></h3>
     <Code>
